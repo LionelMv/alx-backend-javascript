eaff3034a32a8b418bfe4c181199c3366b66ce53
LionelMv <mvangicode@gmail.com>
2023-09-30 20:40:41 +0300

    doc: Add comments
    Describe what the module does

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,3 +1,10 @@
+/*
+ * import the ClassRoom class from 0-classroom.js
+ * Implement a function named initializeRooms
+ * It should return an array of 3 ClassRoom objects
+ * with sizes 19, 20, and 34 (in this order)
+ */
+
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
