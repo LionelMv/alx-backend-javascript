ebc2330c6ee74d503ae0bfc9cc808225488f8c18
LionelMv <mvangicode@gmail.com>
2023-09-30 21:16:20 +0300

    doc: Add comments describe what module does

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,3 +1,12 @@
+/*
+ * implement a class named Currency
+ * constructor attributes: code(String) name(String)
+ * Each attribute must be stored in an 'underscore version'
+ * implement a getter and setter attribute
+ * implement a method named displayFullCurrency that will return
+ * the attributes in the following format name(code).
+ */
+
 export default class Currency {
   constructor(code = '', name = '') {
     this._code = code;
