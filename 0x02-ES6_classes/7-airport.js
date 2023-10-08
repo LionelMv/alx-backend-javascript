3dc78992d6814fcf44251d01955204c85c165d4e
LionelMv <mvangicode@gmail.com>
2023-10-08 19:53:59 +0300

    doc: Add docstring on the file.
    Explain what the code does.

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,3 +1,13 @@
+/*
+ * Implement a class Airport.
+ * Constructor atrributes:
+ * name (string)
+ * code (string)
+ * Each attribute must be stored in an underscore.
+ * The default string description of the class should
+ * return the airport code.
+*/
+
 export default class Airport {
   constructor(name = '', code = '') {
     this._name = name;
