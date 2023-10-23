997b6a39ec94c55514f2aac80f5397e4a5a6890c
LionelMv <mvangicode@gmail.com>
2023-10-23 23:11:05 +0300

    Comply with eslint requirements.

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,13 @@
-import Car from "./10-car.js";
+import Car from './10-car';
 
-export default class EVCar extends Car{
-    constructor(brand, motor, color, range){
-        super(brand, motor, color);
-        this._range = range;
-    }
-    
-    cloneCar() {
-        const NewCar = super.constructor || super[Symbol.species];
-        return new NewCar();
-      }
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const NewCar = super.constructor || super[Symbol.species];
+    return new NewCar();
+  }
 }
