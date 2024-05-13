beec30ed97d410fa42f6578b8d679d00dc04facb
LionelMv <mvangicode@gmail.com>
2024-05-13 20:04:14 +0300

    add 0-console file

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Display a message to the console.
+ *
+ * @param {any} msg - The message to be displayed
+ */
+
+function displayMessage(msg) {
+  console.log(msg);
+}
+
+module.exports = displayMessage;
