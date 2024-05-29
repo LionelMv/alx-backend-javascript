4d6d2fffd2342166cfd9957c0d54d4f532ec172e
LionelMv <mvangicode@gmail.com>
2024-05-29 14:41:32 +0300

    add 9-api

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,37 @@
+const chai = require("chai");
+const expect = chai.expect;
+const request = require("request");
+
+describe("API test cases", () => {
+  const url = "http://localhost:7865";
+
+  it("should return the correct response", (done) => {
+    request.get(`${url}/`, (error, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal("Welcome to the payment system");
+      done();
+    });
+  });
+
+  it("should check the cart route", (done) => {
+    request.get(`${url}/cart/47`, (error, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal("Payment methods for cart 47");
+      done();
+    });
+  });
+
+  it("should check if the id is not a number", (done) => {
+    request.get(`${url}/cart/him`, (error, res) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it("should check if the id is not a number", (done) => {
+    request.get(`${url}/cart/-47`, (error, res) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
